c3be950e1cf4165ddeb0c94a9b31c12ec851cbc9
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-05-25 09:14:36 +0545

    front end new changes

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,14 +2,10 @@ import { Grid, Box, Typography } from "@mui/material";
 function Home() {
   return (
     <Box>
-      <Grid container>
-        <Grid item>
-          <div style={{ backgroundColor: "#F0E0FF" }}>
-            <Typography variant="h4">Winter Collection</Typography>
-          </div>
-        </Grid>
-        <Grid item></Grid>
-        <Grid item></Grid>
+      <Grid>
+        <div style={{ backgroundColor: "#F0E0FF" }}>
+          <Typography variant="h4">Winter Collection</Typography>
+        </div>
       </Grid>
     </Box>
   );
