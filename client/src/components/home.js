36171bbecfe95663f52f56691d36223674b0333b
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-05-25 10:20:18 +0545

    frontend main block added

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,10 +1,32 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box, Typography, Button } from "@mui/material";
 function Home() {
   return (
     <Box>
       <Grid>
         <div style={{ backgroundColor: "#F0E0FF" }}>
-          <Typography variant="h4">Winter Collection</Typography>
+          <Grid
+            container
+            justifyContent="space-around"
+            alignItems="center"
+            paddingTop="50px"
+          >
+            <Grid item>
+              <Typography variant="h6">Small Products</Typography>
+              <Typography variant="h2">Winter Offer</Typography>
+              <Typography variant="h2">2024 Collection </Typography>
+              <Button style={{ border: "1px solid black", color: "black" }}>
+                Shop Now
+              </Button>
+            </Grid>
+            <Grid item>
+              <img
+                src="../images/home-image.png"
+                alt="Home"
+                width="500px"
+                height="500px"
+              />
+            </Grid>
+          </Grid>
         </div>
       </Grid>
     </Box>
