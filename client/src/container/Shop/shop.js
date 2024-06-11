253f8d9d49e1798d370402ca7e2ef4e4ea8e9983
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-11 16:23:14 +0545

    Shop page added

--- client/src/container/Shop/shop.js
+++ client/src/container/Shop/shop.js
@@ -1,5 +1,67 @@
+import { Box, Grid, Typography, Button } from "@mui/material";
 import React from "react";
-
+import data from "../../data.json";
 export const Shop = () => {
-  return <div>shop</div>;
+  return (
+    <Box m={4}>
+      <Grid
+        container
+        spacing={5}
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        {data.map((item, index) => (
+          <Grid item key={index} xs={4}>
+            <img
+              src={item.path}
+              alt={`${item.id}`}
+              width="100%"
+              height="auto"
+            ></img>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="p"
+                style={{
+                  margin: "15px",
+                }}
+              >
+                {item.desc}
+              </Typography>
+
+              {item["old-price"] ? (
+                <div style={{ display: "inline-block" }}>
+                  <span
+                    style={{
+                      textDecoration: "line-through",
+                      margin: "0 15px 0 15px ",
+                    }}
+                  >{`$${item["old-price"]}`}</span>
+                  <Typography
+                    variant="p"
+                    style={{
+                      fontWeight: "500",
+                    }}
+                  >{`$${item["new-price"]}`}</Typography>
+                </div>
+              ) : (
+                <Typography
+                  variant="p"
+                  style={{
+                    fontWeight: "500",
+                  }}
+                >{`$${item["new-price"]}`}</Typography>
+              )}
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
