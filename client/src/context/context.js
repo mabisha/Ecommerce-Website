714d1b02fca5a3c19e51c3c9597111bfdcd91baa
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-12 09:20:17 +0545

    Context file added

---
+++ client/src/context/context.js
@@ -0,0 +1,12 @@
+import React, { createContext } from "react";
+import data from "../data.json";
+export const Shopcontext = createContext(null);
+const ShopContextProvider = (props) => {
+  const contextValue = { data };
+  return (
+    <Shopcontext.Provider value={contextValue}>
+      {props.children}
+    </Shopcontext.Provider>
+  );
+};
+export default ShopContextProvider;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ShopContextProvider from "./context/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>
+  </ShopContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
