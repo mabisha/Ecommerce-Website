2fd191dd6dd3d9e223a341f8e06b1c1d0344e803
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-18 12:23:29 +0545

    no cart item added

--- client/src/container/Cart/cart.js
+++ client/src/container/Cart/cart.js
@@ -2,116 +2,165 @@ import React, { useContext } from "react";
 import { Box, Button, Grid, Typography } from "@mui/material";
 import { Shopcontext } from "../../context/context";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import LocalGroceryStoreOutlinedIcon from "@mui/icons-material/LocalGroceryStoreOutlined";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
   const { data, cartItem, removeFromCart } = useContext(Shopcontext);
-  console.log(cartItem);
-
+  const cartItems = data.find((item, index) => cartItem[item.id] > 0);
+  console.log(cartItems);
   return (
     <Box m={4}>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Typography
-            variant="h6"
-            style={{
-              fontFamily: "Montserrat, sans-serif",
-              fontWeight: "500",
-              margin: "15px 0 15px 0",
-            }}
-          >
-            Your Cart Items
-          </Typography>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            style={{ backgroundColor: "#f9f9f9", padding: "15px" }}
-          >
-            {["Image", "Title", "Price", "Quantity", "Total", "Remove"].map(
-              (header) => (
-                <Grid item xs={2} key={header}>
-                  <Typography
-                    variant="body1"
-                    style={{
-                      fontFamily: "Montserrat, sans-serif",
-                    }}
+      {cartItems ? (
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <Typography
+              variant="h6"
+              style={{
+                fontFamily: "Montserrat, sans-serif",
+                fontWeight: "500",
+                margin: "15px 0 15px 0",
+              }}
+            >
+              Your Cart Items
+            </Typography>
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="center"
+              style={{ backgroundColor: "#f9f9f9", padding: "15px" }}
+            >
+              {["Image", "Title", "Price", "Quantity", "Total", "Remove"].map(
+                (header) => (
+                  <Grid item xs={2} key={header}>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        fontFamily: "Montserrat, sans-serif",
+                      }}
+                    >
+                      {header}
+                    </Typography>
+                  </Grid>
+                )
+              )}
+            </Grid>
+          </Grid>
+          {data.map((item, index) => {
+            if (cartItem[item.id] > 0) {
+              return (
+                <Grid item key={item.id}>
+                  <Grid
+                    container
+                    justifyContent="space-between"
+                    alignItems="center"
+                    spacing={2}
                   >
-                    {header}
-                  </Typography>
+                    <Grid item xs={2}>
+                      <img
+                        src={item.path}
+                        alt="#product"
+                        style={{ width: "100px", height: "auto" }}
+                      />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography
+                        variant="body2"
+                        style={{
+                          fontFamily: "Montserrat, sans-serif",
+                        }}
+                      >
+                        {item.title}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography
+                        variant="body2"
+                        style={{
+                          fontFamily: "Montserrat, sans-serif",
+                        }}
+                      >
+                        ${item.newPrice}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Button
+                        style={{
+                          padding: "5px 5px 5px 5px",
+                          color: "black",
+                          border: "1px solid black",
+                        }}
+                      >
+                        {cartItem[item.id]}
+                      </Button>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography
+                        variant="body2"
+                        style={{
+                          fontFamily: "Montserrat, sans-serif",
+                        }}
+                      >
+                        ${item.newPrice * cartItem[item.id]}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <DeleteForeverIcon
+                        onClick={() => {
+                          removeFromCart(item.id);
+                        }}
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      />
+                    </Grid>
+                  </Grid>
+                  <hr />
                 </Grid>
-              )
-            )}
+              );
+            } else {
+              return null;
+            }
+          })}
+        </Grid>
+      ) : (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          spacing={3}
+        >
+          <Grid item>
+            <LocalGroceryStoreOutlinedIcon
+              style={{ width: "120px", height: "auto" }}
+            ></LocalGroceryStoreOutlinedIcon>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{
+                fontFamily: "Montserrat, sans-serif",
+              }}
+            >
+              No items Found
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Link to="/shop" style={{ textDecoration: "none" }}>
+              <Button
+                style={{
+                  backgroundColor: "black",
+                  color: "white",
+                  padding: "15px",
+                }}
+              >
+                Shop Now
+              </Button>
+            </Link>
           </Grid>
         </Grid>
-        {data.map((item, index) => {
-          if (cartItem[item.id] > 0) {
-            return (
-              <Grid item key={item.id}>
-                <Grid
-                  container
-                  justifyContent="space-between"
-                  alignItems="center"
-                  spacing={2}
-                >
-                  <Grid item xs={2}>
-                    <img
-                      src={item.path}
-                      alt="#product"
-                      style={{ width: "100px", height: "auto" }}
-                    />
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography
-                      variant="body2"
-                      style={{
-                        fontFamily: "Montserrat, sans-serif",
-                      }}
-                    >
-                      {item.title}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography
-                      variant="body2"
-                      style={{
-                        fontFamily: "Montserrat, sans-serif",
-                      }}
-                    >
-                      ${item.newPrice}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Button>{cartItem[item.id]}</Button>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography
-                      variant="body2"
-                      style={{
-                        fontFamily: "Montserrat, sans-serif",
-                      }}
-                    >
-                      ${item.newPrice * cartItem[item.id]}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <DeleteForeverIcon
-                      onClick={() => {
-                        removeFromCart(item.id);
-                      }}
-                      style={{
-                        cursor: "pointer",
-                      }}
-                    />
-                  </Grid>
-                </Grid>
-                <hr />
-              </Grid>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </Grid>
+      )}
     </Box>
   );
 };
