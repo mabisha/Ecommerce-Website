250125d7a2af7aeb11e08e2b143e92aa86632a19
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-18 22:04:31 +0545

    Adding server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const port = 4000;
+const express = require("express");
+const cors = require("cors");
+const pg = require("pg");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+
+app.use(express.json()); // all the request will be parsed to json
+app.use(cors()); // will coonect out app to the port
