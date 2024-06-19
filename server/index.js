13ae8e2389e88bea7d55aca89f691ed295464c14
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-19 22:13:10 +0545

    images storage

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT || 4000;
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const path = require("path");
 const app = express();
 
@@ -12,7 +11,24 @@ app.use(cors()); // Enable CORS
 app.get("/", (req, res) => {
   res.send("Express App is running");
 });
-
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+app.use("/images", express.static("upload/images"));
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("product"), (req, res) => {
+//   res.json({
+//     success: 1,
+//     image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+//   });
+// });
 app.use("/", userRouter);
 
 app.listen(PORT, () => {
