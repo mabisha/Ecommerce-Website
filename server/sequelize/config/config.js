979309fb80ea247dee929f58a9a719dfd2603d62
Mabisha <58455094+mabisha@users.noreply.github.com>
2024-06-23 09:13:53 +0545

    config file edited

--- server/sequelize/config/config.js
+++ server/sequelize/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.REACT_APP_DATABASE_USERNAME,
-    password: process.env.REACT_APP_DATABASE_PASSWORD,
-    database: process.env.REACT_APP_DATABASE_NAME,
-    host: process.env.REACT_APP_DATABASE_HOST,
+    username: process.env.REACT_APP_DB_USERNAME,
+    password: process.env.REACT_APP_DB_PASSWORD,
+    database: process.env.REACT_APP_DB_NAME,
+    host: process.env.REACT_APP_DB_HOST,
     dialect: "postgres",
   },
 };
